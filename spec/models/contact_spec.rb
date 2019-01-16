515f300b881945f7fb780d2eb4c41c39bb5edcc7
Панин М.А <mic@alice.ru>
2019-01-16 17:19:42 +0300

    Add contact modal spec

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,6 @@
 require "spec_helper"
 
 RSpec.describe Contact do
-  it { is_expected.to validate_presence_of(:email) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:message) }
 end
