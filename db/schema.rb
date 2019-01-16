c822fbaaef1e9e60f54c5b744871f507b575599a
Панин М.А <mic@alice.ru>
2019-01-16 15:42:51 +0300

    change to Ruby 2.5.1 and add bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_18_094201) do
+ActiveRecord::Schema.define(version: 2019_01_16_122729) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
   end
 
   create_table "comments", force: :cascade do |t|
