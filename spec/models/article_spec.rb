5f407d448431ed5f548b7bc8f48223d872f60e82
Панин М.А <mic@alice.ru>
2019-01-17 12:04:54 +0300

    Update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -10,13 +10,23 @@ describe Article do
     it { should have_many(:comments) }
   end
 
-  describe "@subject" do
+  describe "#subject" do
     it "returns the article title" do
       # создаем объект article хитрым способом
       article = create(:article, title: 'Lorem Ipsum')
 
       # assert, проверка
       expect(article.subject).to eq 'Lorem Ipsum'
+    end
+  end
+
+  describe "#last_comment" do
+    it "returns the last comment" do
+      # создаем статью, но в это раз с комментами
+      article = create(:article_with_comments)
+
+      # assert, проверка
+      expect(article.last_comment.body).to eq 'comment body 3'
     end
   end
 end
