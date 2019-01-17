80a5bc07df1c51154d041f7a17cb4c89a1559f8d
Панин М.А <mic@alice.ru>
2019-01-17 15:31:36 +0300

    Test length of comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,4 +2,6 @@ require "spec_helper"
 
 describe Comment do
   it { should belong_to(:article) }
+  it { should validate_length_of(:body).is_at_least(1) }
+  it { should validate_length_of(:body).is_at_most(4000) }
 end
