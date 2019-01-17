b29f1fda7bf27097fbea6f2815bae8863ea76138
Панин М.А <mic@alice.ru>
2019-01-17 15:30:24 +0300

    Add length validation in article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true
   validates :text, presence: true
+  validates_length_of :title, minimum: 1, maximum: 140, allow_blank: false
+  validates_length_of :text, minimum: 1, maximum: 4000, allow_blank: false
   has_many :comments
 
   def subject
